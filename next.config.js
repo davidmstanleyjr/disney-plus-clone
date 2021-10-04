4996c68a148d6af3cb0d67c8995bc246896490d4
davidmstanleyjr <davidmstanleyjr@gmail.com>
2021-10-04 16:50:19 -0400

    deploying

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  future: {
-    webpack5: true,
-  },
-  webpack: function (config, options) {
-    config.experiments = {};
-    return config;
-  },
   env: {
 		GRAPH_CMS_TOKEN: process.env.GRAPH_CMS_TOKEN,
     ENDPOINT: process.env.ENDPOINT,
